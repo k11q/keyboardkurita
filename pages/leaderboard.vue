dae052a310487f17326af6ef93f32571de961647
Khairul Haaziq <rhycoz@yahoo.com>
2023-04-20 16:23:21 +0800

    leaderboard page

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -1,81 +1,149 @@
 <template>
-	<div class="max-w-6xl w-full py-10 flex flex-col gap-10 px-10 flex-none">
-		<div
-			v-for="i in 3"
-			class="flex flex-col"
-		>
-			<h2 class="font-mono text-4xl mb-6">
-				10s
-			</h2>
+	<div
+		:class="`flex flex-col items-center flex-grow left-0 right-0 px-6 py-14 w-full max-w-6xl`"
+	>
+	<div class="w-full">
+		<h2 class="font-mono text-4xl mb-6">
+			Top
+		</h2>
+		<template v-if="topWPM && topWPM.length">
 			<table class="w-full font-mono text-neutral-200 flex flex-col gap-0.5 m-0">
-				<thead class="flex flex-col gap-0.5 text-neutral-500 uppercase">
-					<tr
-						class="h-7 grid grid-cols-13 bg-neutral-800/30 [&>*]:border-r-2 [&>*]:border-neutral-900"
-					>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>no</div>
-						</td>
-						<td class="col-span-5 flex items-center gap-4 p-0">
-							<div
-								class="w-[33px] h-7 border-r-2 border-neutral-900"
-							/>
-							<div>user</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>wpm</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>raw</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>acc</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>consis</div>
-						</td>
-						<td class="col-span-3 flex items-center py-0 pr-4 justify-end">
-							<span>date</span>
-						</td>
-					</tr>
-				</thead>
-				<tbody class="flex flex-col gap-0.5">
-					<tr
-						v-for="i in 10"
-						class="h-[33px] grid grid-cols-13 bg-neutral-800/60 even:bg-neutral-800/20 [&>*]:border-r-2 [&>*]:border-neutral-900"
-					>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>{{ i }}</div>
-						</td>
-						<td class="col-span-5 flex items-center gap-4 p-0">
-							<div
-								class="h-[33px] w-[33px] bg-neutral-600 border-r-2 border-neutral-900"
-							/>
-							<div>khairulhaaziq</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>99.4%</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>99.4%</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>99.4%</div>
-						</td>
-						<td class="flex items-center pr-0 py-0 pl-2">
-							<div>99.4%</div>
-						</td>
-						<td class="col-span-3 flex items-center py-0 pr-4 justify-end text-neutral-400">
-							<span>19/2/2027 11:52 PM</span>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+			<thead class="flex flex-col gap-0.5 text-neutral-500 uppercase">
+				<tr
+					class="h-7 grid grid-cols-13 bg-neutral-800/30 [&>*]:border-r-2 [&>*]:border-neutral-900"
+				>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>no</div>
+					</td>
+					<td class="col-span-5 flex items-center gap-4 p-0">
+						<div
+							class="w-[35px] h-7 border-r-2 border-neutral-900"
+						/>
+						<div>user</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>wpm</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>raw</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>acc</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>consis</div>
+					</td>
+					<td class="col-span-3 flex items-center py-0 pr-4 justify-end">
+						<span>date</span>
+					</td>
+				</tr>
+			</thead>
+			<tbody class="flex flex-col gap-0.5">
+				<tr
+				v-for="session, index in topWPM"
+								:key="session.id"
+					class="h-[33px] grid grid-cols-13 bg-neutral-800/60 even:bg-neutral-800/20 [&>*]:border-r-2 [&>*]:border-neutral-900"
+				>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>{{ index+1 }}</div>
+					</td>
+					<td class="col-span-5 flex items-center gap-4 p-0">
+						<div
+							class="h-[33px] w-[35px] bg-neutral-600 border-r-2 border-neutral-900"
+						/>
+						<div>{{
+									session.username
+								}}</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>{{
+									session.wpm.toFixed(
+										1
+									)
+								}}</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>{{
+									session.raw.toFixed(
+										1
+									)
+								}}</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>{{
+									session.accuracy.toFixed(
+										1
+									)
+								}}%</div>
+					</td>
+					<td class="flex items-center pr-0 py-0 pl-2">
+						<div>{{
+									session.consistency.toFixed(
+										1
+									)
+								}}%</div>
+					</td>
+					<td class="col-span-3 flex items-center py-0 pr-4 justify-end text-neutral-400">
+						<span class="line-clamp-1">{{
+									format(
+										new Date(
+											session.end_time
+										),
+										'dd/MM/yyyy hh:mm a'
+									)
+								}}</span>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		</template>
+	</div>
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { format } from "date-fns";
 definePageMeta({
-	middleware: 'auth',
+middleware: 'auth',
 });
+//db & auth
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+
+// readonly
+const PROFILE = useState('profile');
+const USERNAME: globalThis.Ref<string> = computed(() => {
+return PROFILE.value ? PROFILE.value.username : "username";
+});
+
+const { data: pastSessions } = await useLazyAsyncData('pastSessions', async () => {
+const { data } = await client.from('sessions_with_profile').select().limit(10).order('id',{ascending: false})
+return data
+})
+
+const {data:topWPM} = await useLazyAsyncData('topWPM', async () => {
+const { data } = await client.from('sessions_with_profile').select().limit(10).order('wpm',{ascending: false})
+return data
+})
+
+watchEffect(async () => {
+if (user.value) {
+	PROFILE.value = await getProfile(user.value.id);
+}
+});
+
+async function getProfile(userId: string) {
+const { data, error } = await client
+	.from("profile")
+	.select()
+	.eq("user_id", userId)
+	.single();
+if (error) {
+	console.log(error);
+	console.log(
+		"no profile for users found. user needs to setup profile"
+	);
+}
+return data;
+}
 </script>
